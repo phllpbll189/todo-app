f281978171878c495f9e9c73a63a9b7bb0cb1f7d
phllpbll189 <phillip@devbell.net>
2023-04-30 11:10:27 -0500

    Started adding JWTs to the first api.
    TODO: create database to store usernames and passwords
    Utilize Google SSO for Todos

--- server/index.js
+++ server/index.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const { request } = require('http');
+const https = require('https');
+
 const app = express();
+const PORT = 8000;
+
+//login if not logged in
+//redirect to todo page and send profile data
+//they are logged in if they send a valid JWT
+
+app.listen(PORT, (err) => {
+    if(err) console.log(err);
+    console.log("App is running now");
+})
+
+app.use(AppendCookie)
 
 app.get('/', (req, res) => {
-
+    console.log(req);
+    res.send("helloworld");
 })
+
+function AppendCookie(req, res, next) {
+    const token = jwt.sign({email: "Phllpbll@fuck.com"}, 'insertabetterkeyhere');
+    res.cookie("access_token", token, {
+        httpOnly: true,
+        secure: "production",
+    }).status(200)
+    .json({message: "logged in successfully"})
+
+    next();
+}
